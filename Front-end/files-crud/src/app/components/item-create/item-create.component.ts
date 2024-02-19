60050006cca0801f216172a4d482faf8e27495c2
Your Name <sinkondeluvmo11@gmail.com>
2024-02-19 15:37:00 +0200

    Remove the comments

--- Front-end/files-crud/src/app/components/item-create/item-create.component.ts
+++ Front-end/files-crud/src/app/components/item-create/item-create.component.ts
@@ -1,30 +1,21 @@
-// item-create.component.ts
-
-// Import required Angular modules and the ApiService
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 
-// Decorator and component definition
 @Component({
   selector: 'app-item-create',
   templateUrl: './item-create.component.html',
   styleUrls: ['./item-create.component.css'],
 })
 export class ItemCreateComponent implements OnInit {
-  // Define an object to store new item data including file
   newItem: any = {
     name: '',
     description: '',
     image: null,
   };
 
-  // Inject the ApiService in the constructor
   constructor(private apiService: ApiService) {}
-
-  // Lifecycle hook, called when the component is initialized
   ngOnInit(): void {}
 
-  // Method to create a new item with file upload
   createItem(): void {
     const formData = new FormData();
     formData.append('name', this.newItem.name);
@@ -33,11 +24,9 @@ export class ItemCreateComponent implements OnInit {
 
     this.apiService.createItem(formData).subscribe(() => {
       console.log('Item created successfully');
-      // Optionally, navigate to the item list or reset the form
+
     });
   }
-
-  // Method to handle file input change
   handleFileInput(event: any): void {
     this.newItem.image = event.target.files[0];
   }
